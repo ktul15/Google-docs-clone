c0a8381bb6120e3ddd4c60a1e4b17525f38bc4f3
Ketul <ketulm8@gmail.com>
2021-08-18 11:57:59 +0530

    Add toolbar options to text editor

--- client/src/TextEditor.jsx
+++ client/src/TextEditor.jsx
@@ -2,16 +2,31 @@ import React, { useRef, useEffect } from 'react'
 import Quill from 'quill'
 import 'quill/dist/quill.snow.css'
 
+const TOOLBAR_OPTIONS = [
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+  [{ font: [] }],
+  [{ list: 'ordered' }, { list: 'bullet' }],
+  ['bold', 'italic', 'underline'],
+  [{ color: [] }, { background: [] }],
+  [{ script: 'sub' }, { script: 'super' }],
+  [{ align: [] }],
+  ['image', 'blockquote', 'code-block'],
+  ['clean'],
+]
+
 export default function TextEditor() {
   // const wrapperRef = useRef()
 
   useEffect(() => {
     // const editor = document.createElement('div')
     // wrapperRef.current.append(editor)
-    new Quill('#container', { theme: 'snow' })
+    new Quill('.container', {
+      theme: 'snow',
+      modules: { toolbar: TOOLBAR_OPTIONS },
+    })
     // return () => {
     // wrapperRef.innerHTML = ''
     // }
   }, [])
-  return <div id="container"></div>
+  return <div className="container"></div>
 }
